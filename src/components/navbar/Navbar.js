7c2c252828271289dbd77aea0f7569a4da43b60f
Bassel <basselkanso82@gmail.com>
2020-05-24 19:42:07 +0300

    making styles in scss

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { Link } from "gatsby"
 import './Navbar.scss';
 
-const activeStyle = { 
-    textDecoration:"underline 2px", 
-    textUnderlinePosition: "under"
-}
-
 export default function Navbar ({children}){
     return (
         <React.Fragment>
         <div className="navbar">
-            <Link activeStyle={activeStyle} className = "navbar--link" to="/" >About Me</Link>
-            <Link activeStyle={activeStyle} className = "navbar--link" to="/portfolio/portfolio">Portfolio</Link>
-            <Link activeStyle={activeStyle} className = "navbar--link" to="/contact/contact">Contact Me</Link>
-            <Link activeStyle={activeStyle} className = "navbar--link" to="/blog/blog">Blog</Link>
+            <Link activeClass="navbar--link__active" className = "navbar--link" to="/" >About Me</Link>
+            <Link activeClass="navbar--link__active" className = "navbar--link" to="/portfolio/portfolio">Portfolio</Link>
+            <Link activeClass="navbar--link__active" className = "navbar--link" to="/contact/contact">Contact Me</Link>
+            <Link activeClass="navbar--link__active" className = "navbar--link" to="/blog/blog">Blog</Link>
         </div>
         {children}
         </React.Fragment>
