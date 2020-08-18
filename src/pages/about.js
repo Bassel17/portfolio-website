0560584ba8511bc22770c1dbe3334b823688909e
Bassel <basselkanso82@gmail.com>
2020-08-19 01:04:46 +0300

    feat:

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { graphql, useStaticQuery } from "gatsby"
 import Navbar from '../components/navbar/Navbar.js';
 import AboutCard from '../components/aboutCard/AboutCard';
-import Img from "gatsby-image"
+import Img from "gatsby-image";
+import { Icon } from '@iconify/react';
+import githubIcon from '@iconify/icons-cib/github';
+import instagramIcon from '@iconify/icons-cib/instagram';
+import linkedinIcon from '@iconify/icons-cib/linkedin-in';
 import './style/about.scss'
 
 const About = () => {
@@ -28,9 +32,9 @@ const About = () => {
                 <h1>Bassel Kanso</h1>
                 <AboutCard/>
                 <div className="about--info--footer">
-                    <div>instagram</div>
-                    <div>linkedin</div>
-                    <div>github</div>
+                    <a href="#" target="_blank"><Icon icon={instagramIcon} /></a>
+                    <a href="#" target="_blank"><Icon icon={linkedinIcon} /></a>
+                    <a href="#" target="_blank"><Icon icon={githubIcon} /></a>
                 </div>
             </div>
         </div>
