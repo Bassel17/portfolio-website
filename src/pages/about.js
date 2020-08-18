714f75dae34cf143ed527c76898acd7fd189942a
Bassel <basselkanso82@gmail.com>
2020-08-19 01:15:53 +0300

    fix: fixed problem with node-sass package

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import { Icon } from '@iconify/react';
 import githubIcon from '@iconify/icons-cib/github';
 import instagramIcon from '@iconify/icons-cib/instagram';
 import linkedinIcon from '@iconify/icons-cib/linkedin-in';
+import twitterIcon from '@iconify/icons-cib/twitter';
 import './style/about.scss'
 
 const About = () => {
@@ -32,9 +33,10 @@ const About = () => {
                 <h1>Bassel Kanso</h1>
                 <AboutCard/>
                 <div className="about--info--footer">
-                    <a href="#" target="_blank"><Icon icon={instagramIcon} /></a>
-                    <a href="#" target="_blank"><Icon icon={linkedinIcon} /></a>
-                    <a href="#" target="_blank"><Icon icon={githubIcon} /></a>
+                    <a href="https://www.instagram.com/bassel_k/" target="_blank" rel="noreferrer"><Icon icon={instagramIcon} /></a>
+                    <a href="https://www.linkedin.com/in/bassel-kanso-937649128/" target="_blank" rel="noreferrer"><Icon icon={linkedinIcon} /></a>
+                    <a href="https://www.github.com/bassel17/" target="_blank" rel="noreferrer"><Icon icon={githubIcon} /></a>
+                    <a href="https://www.github.com/bassel17/" target="_blank" rel="noreferrer"><Icon icon={twitterIcon} /></a>
                 </div>
             </div>
         </div>
